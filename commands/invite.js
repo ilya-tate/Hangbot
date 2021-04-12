0c2b89a5626f609a8f3b3336da3716467b1b8234
Ilya Tate <ilyatate@gmail.com>
2021-04-11 21:00:29 -0700

    Fixed invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,9 +1,9 @@
+const { botInviteLink } = require('../config.json');
+
 module.exports = {
   name: 'invite',
   description: 'Creates invite link for bot',
   execute(message, arguments) {
-    const { botInviteLink } = require('./config.json')
-
     message.channel.send(botInviteLink);
   }
 }
